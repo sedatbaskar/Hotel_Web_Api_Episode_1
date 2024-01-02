51123ff8f3d4797243ed0006bd0ea4ef55c55d77
Sedat Başkar <sedatbaskar@gmail.com>
2024-01-03 00:49:38 +0300

    Create Staff

--- HotelProject.WebUI/Controllers/StaffController.cs
+++ HotelProject.WebUI/Controllers/StaffController.cs
@@ -1,12 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class StaffController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StaffController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/Staff");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
+                return View(values);
+            }
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStaff(AddStaffViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var responseMessage = await client.PostAsync("https://localhost:7000/api/Staff", stringContent);
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    // Handle unsuccessful response (e.g., show error message)
+                    // return a specific view or message to indicate the failure
+                    return View("ErrorView");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                // Handle connection errors (e.g., log the error or display a message to the user)
+                // return a specific view or message to indicate the failure
+                return View("ConnectionErrorView");
+            }
+        }
+
+
     }
 }
